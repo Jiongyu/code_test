99f100bb8e4e6d50c3942a22166058ab5e9c2a44
Jiongyu <35024339@qq.com>
2022-07-23 10:02:38 +0800

    添加opencv demo

---
+++ opencv_test/opencv_test.cpp
@@ -0,0 +1,21 @@
+#include<opencv2/features2d/features2d.hpp>  
+#include<opencv2/opencv.hpp>
+ 
+using namespace cv;
+ 
+int main( int argc, char** argv )
+{
+    Mat image;
+    image = imread("../1.png", 1);
+ 
+    // if( argc != 2 || !image.data )
+    // {
+    //     printf( "No image data \n" );
+    //     return -1;
+    // }
+
+    imshow( "Display Image", image );
+    waitKey(0);
+ 
+    return 0;
+}
